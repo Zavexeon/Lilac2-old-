30f8c029a3587b1c5b437c7303b65b5aee62ed47
Codemonkey51 <62217716+Codemonkey51@users.noreply.github.com>
2020-11-06 12:13:39 -0800

    Update it
    Not finished but progress.

--- lilac2/bot/modules/dadjokes.js
+++ lilac2/bot/modules/dadjokes.js
@@ -1,1 +1,14 @@
-
+module.exports = (bot, cache) => {
+    return {
+        name: 'dad jokes',
+        description: 'eveyone likes dad jokes, this is a module to give you some',
+        commands: {
+            im: {
+                description: 'Hi dad im bored. dad: Hi bored',
+                minArgs: 1,
+                maxArgs: 1,
+              
+            }
+        }
+    }
+}
